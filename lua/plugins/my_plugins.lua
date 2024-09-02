c1a4cde644f16253ddaf0ab1fd9877a1ea67e171
Jeremy Sigrist <jsigrist1@gmail.com>
2024-09-02 14:24:10 -0700

    Try and install json5 parser

---
+++ lua/plugins/my_plugins.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'machakann/vim-sandwich',
+    name = "vim-sandwich",
+    opts = {
+    },
+    config = function ()
+    end,
+  },
+  -- {
+  --   "Joakker/lua-json5",
+  --   lazy = false,
+  --   build = vim.fn.has("win32") == 1 and "powershell ./install.ps1" or "./install.sh",
+  -- },
+}
