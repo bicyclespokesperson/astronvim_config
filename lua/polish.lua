83fb735ea6b24301e1f7ccf3474022e6d821b8d1
Jeremy Sigrist <jsigrist1@gmail.com>
2025-12-03 12:03:19 -0800

    Reapply "Add workaround for treesitter merge conflict error"
    This reverts commit 181c7046f1f88157e82b98432b9d8b8bd157b7ff.

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,13 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+-- Disable treesitter highlighting for files with merge conflicts
+-- (workaround for nvim-treesitter nil node bug)
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local lines = vim.api.nvim_buf_get_lines(0, 0, 100, false)
+    for _, line in ipairs(lines) do
+      if line:match("^<<<<<<<") then
+        vim.treesitter.stop(0)
+        return
+      end
+    end
+  end,
+})
